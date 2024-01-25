a6a1b389d661163f45e912b936f2d79e98a72ddc
Arthur Gibert <arthurgibert@free.fr>
2024-01-25 15:14:57 +0100

    Ajouter méthode pour chrono

--- src/SwingFile.java
+++ src/SwingFile.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
@@ -13,6 +15,9 @@ public class SwingFile extends JFrame implements Runnable { // On implémente l'
 	private static final long serialVersionUID = 1L; // valeur qui identifie de manière unique les versions sérialisées de la classe.
 	private static long delai = 1000; 	// Délai (en milisecondes) entre les mises à jour de l'horloge
 	private JLabel horlogeLabel;				// Composant JLabel pour afficher l'heure
+	private JTextArea chrono;
+	private long elapsedTime = 0;
+	private Timer timer;
 	
 	public SwingFile() {
 		// Configuration de notre fenêtre:
@@ -47,16 +52,50 @@ public class SwingFile extends JFrame implements Runnable { // On implémente l'
 		panelNorth.add(horlogeLabel, SwingConstants.CENTER);
 		this.getContentPane().add(panelNorth, BorderLayout.NORTH);
 	}
+	
+	public void startChrono() {
+		timer = new Timer((int) (delai), new ActionListener() { // Timer est créer avec un délai spécifier en miliseconde entre chaque exécution de l'action.
+			public void actionPerformed(ActionEvent e) {
+		        elapsedTime = elapsedTime + delai;
+		        long hours = elapsedTime / 3600000; // On calcule le nombre d'heure dans elapsedTime
+		        long minutes = (elapsedTime % 3600000) / 60000; // On calcule le nombre de minute en prenant le reste et en divisant / 60000
+		        long seconds = (elapsedTime % 60000) / 1000; // On calcule le nombre de seconde en prenant le reste et en divisant / 1000
+
+		        String formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+		        chrono.setText(formattedTime);
+			}
+		});
+		timer.start();
+	}
+	
+	public void stopChrono() {
+		if(timer != null) {
+			timer.stop();
+		}
+	}
+	
 	// Méthode pour afficher le chrono:
 	public void initCenter() {
 		JPanel panelCenter = new JPanel();
 		panelCenter.setLayout(new FlowLayout(FlowLayout.LEFT));
 		
-		JButton startChrono = new JButton("Start Chrono");
+		JButton startChrono = new JButton("Start/Stop Chrono");
+		startChrono.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(timer == null) {
+					startChrono();
+				}else {
+					stopChrono();
+				}
+				
+			}
+		});
 		startChrono.setPreferredSize(new Dimension(160 , 80));
 		panelCenter.add(startChrono, BorderLayout.WEST);
 		
-		JTextArea chrono = new JTextArea("00:00:00");
+		chrono = new JTextArea("00:00:00");
 		chrono.setPreferredSize(new Dimension(160 , 80));
 		panelCenter.add(chrono, BorderLayout.EAST);
 		this.getContentPane().add(panelCenter, BorderLayout.CENTER);
